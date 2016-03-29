e252c1f5e56ed50f686859d0ea341a49218c17cc
Mike Stout <stout@uselessopinions.com>
2016-03-28 21:08:04 -0700

    Added max owned for buyables

--- Assets/Buyable.cs
+++ Assets/Buyable.cs
@@ -7,10 +7,16 @@ public class Buyable : MonoBehaviour
 {
     public float baseCost = 1.0f;
     public float costMult = 1.15f; //1.07-1.15 is a good range
-    public float numberOwned = 0;
+    public int numberOwned = 0;
+    public int maxOwned = int.MaxValue;
 
     public Button theButton = null;
     public Text priceText = null;
+    public Text numberOwnedText = null;
+    public Text maxOwnedText = null;
+
+    public MonoBehaviour[] disableBehaviorsOnMax = null;
+    public GameObject[] disableObjectsOnMax = null;
 
     public float _debugCurrentCost = 0;
 
@@ -20,16 +26,42 @@ public class Buyable : MonoBehaviour
         
         UpdateButtonInfo();
         _debugCurrentCost = Cost;
-
+        
     }//Update
 
+    void LateUpdate()
+    {
+        if(numberOwned >= maxOwned)
+        {
+            ShutDown();
+        }//if
+    }//lateUpdate
+
+    private void ShutDown()
+    {
+        for (int i = 0; i < disableObjectsOnMax.Length; i++)
+            disableObjectsOnMax[i].SetActive(false);
+
+        for (int i = 0; i < disableBehaviorsOnMax.Length; i++)
+            disableBehaviorsOnMax[i].enabled = false;
+
+    }//ShutDown
     public void UpdateButtonInfo()
     {
-        if (priceText != null)
-            priceText.text = "Cost $:" + string.Format("{0:n2}",Math.Round(Cost, 1));
+        if (maxOwnedText != null)
+            maxOwnedText.text = string.Format("{0}", maxOwned);
+
+        if (numberOwnedText != null)
+            numberOwnedText.text = string.Format("{0}", numberOwned);
+
+        if (priceText != null && numberOwned < maxOwned)
+        {
+            priceText.text = "$" + string.Format("{0:n2}", Math.Round(Cost, 1));
+            //priceText.enabled = numberOwned < maxOwned;
+        }//if
 
         if (theButton != null)
-            theButton.interactable = (GameManager.self.Resource >= Cost);
+            theButton.interactable = ((GameManager.self.Resource >= Cost) && (numberOwned < maxOwned));
     }
 
     public float Cost

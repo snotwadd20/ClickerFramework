b7d0ea35727cc5b116425b02a26b2675b1ecbe39
Mike Stout <stout@uselessopinions.com>
2016-03-31 22:58:26 -0700

    Updating generators to support progressbars (if the progress bar implements "IValue"

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -10,11 +10,19 @@ public class Generator : MonoBehaviour
     public float generateAfterSeconds = 1.0f;
 
     public bool startImmediately = false;
+    public bool isStarted = false;
 
     //public float levelUpMultiplier = 0.1f;
     public int currentLevel = 0;
 
+    public GameObject progressBar = null;
+    private IValue _pbValue = null;
+    //public MonoBehaviour behavior = null;
+
     public bool printTick = false;
+
+    public float progress = 0.0f;
+
     public void OnPurchased()
     {
         if (!startImmediately && currentLevel == 0)
@@ -23,20 +31,42 @@ public class Generator : MonoBehaviour
         currentLevel++;
     }//OnPurchase
 
+    //public MonoBehaviour behavior = null;
 	// Use this for initialization
 	void Start ()
     {
         GameManager.self.AddResource(bonusCash);
+        
+        if(progressBar != null)
+        {
+            _pbValue = progressBar.GetComponentInChildren<IValue>();
+            //print(_pbValue);
+            //behavior = (MonoBehaviour)_pbValue;
+            //behavior = (MonoBehaviour)_pbValue;
+            if (_pbValue == null)
+            {
+                Debug.LogWarning("Progress bar game object does not have an attached script that implements IValue");
+            }//else
+        }
 
-        if(startImmediately)
+        if (startImmediately)
             StartCoroutine(Tick());
-
     }
 
-   
+    void Update()
+    {
+        if (_pbValue != null)
+        {
+            _pbValue.Value = progress;
+        }//if
+
+    }//Update
+
 
     public IEnumerator Tick()
     {
+        float timer = 0.0f;
+        isStarted = true;
         while (true)
         {
             float resources = (resourcePerSecond * currentLevel + GameManager.self.TotalTickMultiplier);
@@ -45,8 +75,23 @@ public class Generator : MonoBehaviour
             if (printTick)
                 print(name + " -> TICK (" + generateAfterSeconds + "s) for $" + resources);
 
-            yield return new WaitForSeconds(generateAfterSeconds);
+
+
+            while (timer < generateAfterSeconds)
+            {
+                timer += Time.deltaTime;
+                progress = timer / generateAfterSeconds;
+                yield return new WaitForEndOfFrame();
+            }//while
+            timer = 0;
+            //yield return new WaitForSeconds(generateAfterSeconds);
+            yield return new WaitForEndOfFrame();
+
+
         }//while
 
     }//Tick
+
+    
+    
 }

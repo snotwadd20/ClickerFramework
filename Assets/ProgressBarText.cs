298934a46a4f5583703656d73f1ef520c607977c
Mike Stout <stout@uselessopinions.com>
2016-03-31 22:58:56 -0700

    A progress bar that just counts a number up from 0. Works both with percentages and factors.

--- Assets/ProgressBarText.cs
+++ Assets/ProgressBarText.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class ProgressBarText : MonoBehaviour {
+public class ProgressBarText : MonoBehaviour, IValue
+{
+    public enum DisplayType { Percent, Factor }
+    public DisplayType type = DisplayType.Percent;
 
-	// Use this for initialization
-	void Start () {
-	
+    public Text displayProgress = null;
+    public string prefix = "";
+    public string postFix = "%";
+    public int decimalPlaces = 0;
+
+    private float _value = 0;
+    public float Value
+    {
+        get { return _value; }
+        set
+        {
+            if(displayProgress != null)
+                displayProgress.text = string.Format("{0}{1}{2}", prefix, (type == DisplayType.Percent ? Mathf.RoundToInt(100 * value) : 1 * value), postFix);
+
+            _value = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (displayProgress == null)
+            displayProgress = gameObject.GetComponent<Text>();
+
+        if(displayProgress == null)
+        {
+            Debug.LogWarning("ProgressBarText script is not linked to or placed on an object with a Text object. Disabling.");
+            enabled = false;
+        }
 	}
-	
+    
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+            
+        
 	}
-}
+}//ProgressBarText

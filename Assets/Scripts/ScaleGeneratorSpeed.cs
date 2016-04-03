65380e592286f0d0f0f5cf5d92ccbd85e3b3c1d3
Mike Stout <stout@uselessopinions.com>
2016-04-03 14:31:46 -0700

    This script, on Activate or OnClick messages, scales up the speed of a generator

---
+++ Assets/Scripts/ScaleGeneratorSpeed.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleGeneratorSpeed : MonoBehaviour
+{
+    public Generator generator = null;
+    public float speedMultiplier = 2.0f;
+    
+    void Start()
+    {
+        if(generator == null)
+        {
+            Debug.LogError(this.GetType() + ": No generator attached to" + gameObject.name + ". Disabling.");
+            enabled = false;
+        }//if
+    }//Start
+
+    public void Activate() { OnPurchased(); }
+    public void OnPurchased()
+    {
+        generator.tickDurationInSeconds /= speedMultiplier;
+    }//OnPurchased
+}

26fac93c45cd7604c33adc622d269a352b9aafb5
Mike Stout <stout@uselessopinions.com>
2016-04-08 19:14:48 -0700

    Allow objects to be unlocked sequentially

---
+++ Assets/Scripts/BuyableChain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuyableChain : ActivationChain
+{
+	public void OnPurchased ()
+    {
+        if(nextLink != null)
+            nextLink.gameObject.SetActive(true);
+
+        UnlockLinks();
+
+        gameObject.SetActive(false); 
+	}//OnPurchased
+}//BuyableChain
